1c766066b6ac8367898864ad6fe111571af150ba
cthrasher <cthrasher@gmail.com>
2023-06-12 17:23:21 -0400

    Update kingdom.py

--- km/tables/kingdom.py
+++ km/tables/kingdom.py
@@ -9,3 +9,27 @@ kingdom_size = pd.DataFrame(
   [ 100, np.nan, 'Dominion', 'd12', 4, 20 ]],
   columns=('Size_Low','Size_High','Type of Nation','Resource Die','Control DC Modifier','Commodity Storage')
 )
+
+kingdom_advancement = pd.DataFrame(
+  [[ 1, 14 ],
+  [ 2, 15 ],
+  [ 3, 16 ],
+  [ 4, 18 ],
+  [ 5, 20 ],
+  [ 6, 22 ],
+  [ 7, 23 ],
+  [ 8, 24 ],
+  [ 9, 26 ],
+  [ 10, 27 ],
+  [ 11, 28 ],
+  [ 12, 30 ],
+  [ 13, 31 ],
+  [ 14, 32 ],
+  [ 15, 34 ],
+  [ 16, 35 ],
+  [ 17, 36 ],
+  [ 18, 38 ],
+  [ 19, 39 ],
+  [ 20, 40 ]],
+  columns=('Level','Control DC')
+)
